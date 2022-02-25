43f1e40349a63624f00540d99dde35ee5d2a8fd3
Beargame(곰겜) <81502235+beargame123@users.noreply.github.com>
2022-02-25 19:56:17 +0900

    Delete main/java/com/example/mbtogether/domain directory

--- main/java/com/example/mbtogether/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mbtogether.domain.repository;
-
-import com.example.mbtogether.domain.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String>{
-}
