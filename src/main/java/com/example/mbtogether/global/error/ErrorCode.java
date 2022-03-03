5571e32048544e1d5ba188b5c94064298362fb7a
이정윤 <dlwjddbs14@naver.com>
2022-03-03 19:29:39 +0900

    ♻ :: 글로벌 예외처리 설정

--- src/main/java/com/example/mbtogether/global/error/ErrorCode.java
+++ src/main/java/com/example/mbtogether/global/error/ErrorCode.java
@@ -1,16 +1,19 @@
 package com.example.mbtogether.global.error;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
-    INVALID_INPUT_VALUE(400, "Invalid Input Value");
+    INVALID_INPUT_VALUE(400, "Invalid Input Value"),
+
+    OTHER_SERVER_BAD_REQUEST(400, "Bad Request."),
+    OTHER_SERVER_UNAUTHORIZED(401, "Unauthorized token."),
+    OTHER_SERVER_EXPIRED_TOKEN(401, "Expired token."),
+    OTHER_SERVER_FORBIDDEN(403, "Forbidden Consumer.");
 
     private final int httpStatus;
     private final String message;
