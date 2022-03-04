b27e08a0103dcd49f265f35c9d3c9b8571782de1
이정윤 <dlwjddbs14@naver.com>
2022-03-04 23:55:13 +0900

    📑 :: 카카오 로그인 링크 dto

---
+++ src/main/java/com/example/mbtogether/domain/user/api/dto/response/KakaoLinkResponse.java
@@ -0,0 +1,11 @@
+package com.example.mbtogether.domain.user.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoLinkResponse {
+
+    private final String kakaoLink;
+}
