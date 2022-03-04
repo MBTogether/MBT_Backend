ef9b7aa1c4a1fc23eb1beb2a2e97503362f5e823
이정윤 <dlwjddbs14@naver.com>
2022-03-04 23:55:28 +0900

    📑 :: 카카오 로그인 링크

--- src/main/java/com/example/mbtogether/domain/user/api/UserController.java
+++ src/main/java/com/example/mbtogether/domain/user/api/UserController.java
@@ -1,4 +1,19 @@
 package com.example.mbtogether.domain.user.api;
 
+import com.example.mbtogether.domain.user.api.dto.response.KakaoLinkResponse;
+import com.example.mbtogether.domain.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/auth/kakao")
+    public KakaoLinkResponse getKakaoLink() {
+        return userService.getKakaoLink();
+    }
 }
--- src/main/java/com/example/mbtogether/domain/user/service/UserService.java
+++ src/main/java/com/example/mbtogether/domain/user/service/UserService.java
@@ -1,4 +1,31 @@
 package com.example.mbtogether.domain.user.service;
 
+import com.example.mbtogether.domain.user.api.dto.response.KakaoLinkResponse;
+import com.example.mbtogether.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private static final String KAKAO_LOGIN_LINK = "https://kauth.kakao.com/oauth/authorize";
+
+    @Value("${oauth.kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${oauth.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    public KakaoLinkResponse getKakaoLink() {
+        return new KakaoLinkResponse(KAKAO_LOGIN_LINK +
+                "?client_id=" + kakaoClientId +
+                "&redirect_uri=" + URLEncoder.encode(kakaoRedirectUri, StandardCharsets.UTF_8) +
+                "&response_type=code"
+        );
+    }
 }
