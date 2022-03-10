a7b83f510ab687f9d35a8c6a4c946cb7fad6f822
Beargame(곰겜) <81502235+beargame123@users.noreply.github.com>
2022-03-10 18:52:06 +0900

    Delete domain directory

--- domain/report/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mbtogether.domain.report.repository;
-
-import com.example.mbtogether.domain.report.entity.CommentReport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<CommentReport, Integer> {
-}
