17ed8b92d0b8f50e5cef22fcaea4f40dc517f4f9
Beargame(곰겜) <81502235+beargame123@users.noreply.github.com>
2022-03-10 19:53:19 +0900

    Delete src/main/java/com/example/mbtogether/domain/comment directory

--- src/main/java/com/example/mbtogether/domain/comment/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mbtogether.domain.comment.repository;
-
-import com.example.mbtogether.domain.comment.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
