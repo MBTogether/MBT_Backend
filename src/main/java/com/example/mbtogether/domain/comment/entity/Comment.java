e3121167ecfea12917c57430b965070c1d60509d
Beargame(곰겜) <81502235+beargame123@users.noreply.github.com>
2022-03-13 03:00:45 +0900

    ♻️코드 수정

---
+++ src/main/java/com/example/mbtogether/domain/comment/entity/Comment.java
@@ -0,0 +1,45 @@
+package com.example.mbtogether.domain.comment.entity;
+
+import com.example.mbtogether.domain.post.entity.Post;
+import com.example.mbtogether.domain.report.entity.CommentReport;
+import com.example.mbtogether.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="COMMENT")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @OneToMany(mappedBy = "comment", cascade = CascadeType.REMOVE)
+    private List<CommentReport> commentReports;
+
+    public Comment(String comment, Integer id) {
+        this.comment = comment;
+        this.id = id;
+    }
+}
