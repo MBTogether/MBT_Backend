1fefcb9a47ffa677c6556d5b4c07b23ca88be8fa
Beargame(곰겜) <81502235+beargame123@users.noreply.github.com>
2022-03-13 03:10:34 +0900

    ♻️코드 수정

--- src/main/java/com/example/mbtogether/domain/domain/good/service/CommentGoodService.java
+++
@@ -1,7 +0,0 @@
-package com.example.mbtogether.domain.good.service;
-
-public interface CommentGoodService {
-    void insertGood(int commentId);
-
-    void deleteGood(String commentId, int userId);
-}
--- src/main/java/com/example/mbtogether/domain/domain/post/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.mbtogether.domain.post.repository;
-
-import com.example.mbtogether.domain.post.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
