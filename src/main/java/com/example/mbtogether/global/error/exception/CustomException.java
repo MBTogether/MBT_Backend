cea3a7f856b991cffec4f3a9f037f7d592d9fca1
Beargame(곰겜) <81502235+beargame123@users.noreply.github.com>
2022-03-13 02:08:51 +0900

    ♻️코드 수정

---
+++ src/main/java/com/example/mbtogether/global/error/exception/CustomException.java
@@ -0,0 +1,12 @@
+package com.example.mbtogether.global.error.exception;
+
+import com.example.mbtogether.global.error.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+}
---
+++ src/main/java/com/example/mbtogether/global/security/detail/AdminUserDetail.java
@@ -0,0 +1,55 @@
+package com.example.mbtogether.global.security.detail;
+
+import com.example.mbtogether.domain.user.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@AllArgsConstructor
+public class AdminUserDetail implements UserDetails {
+
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> auth = new ArrayList<>();
+        auth.add(new SimpleGrantedAuthority("ADMIN"));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
