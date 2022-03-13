9afd16feb3196b34070a27718523a15136903537
이정윤 <dlwjddbs14@naver.com>
2022-03-13 17:21:11 +0900

    📑 :: google 링크 dto

---
+++ src/main/java/com/example/mbtogether/domain/user/api/dto/response/GoogleLinkResponse.java
@@ -0,0 +1,11 @@
+package com.example.mbtogether.domain.user.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class GoogleLinkResponse {
+
+    private final String googleLink;
+}
