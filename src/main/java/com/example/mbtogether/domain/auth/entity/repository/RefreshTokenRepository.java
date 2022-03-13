b4b14b2fd57f43aceebf2131a8c3fcc1a919605c
이정윤 <dlwjddbs14@naver.com>
2022-03-13 17:19:24 +0900

    📑 :: refresh token

---
+++ src/main/java/com/example/mbtogether/domain/auth/entity/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.mbtogether.domain.auth.entity.repository;
+
+import com.example.mbtogether.domain.auth.entity.RefreshToken;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
+}
