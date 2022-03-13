56342c5d350fa61b602389acaf35d25ee30f219e
이정윤 <dlwjddbs14@naver.com>
2022-03-13 17:21:26 +0900

    📑 :: auth api

--- src/main/java/com/example/mbtogether/domain/user/api/UserController.java
+++ src/main/java/com/example/mbtogether/domain/user/api/UserController.java
@@ -1,14 +1,12 @@
 package com.example.mbtogether.domain.user.api;
 
+import com.example.mbtogether.domain.user.api.dto.request.LoginRequest;
 import com.example.mbtogether.domain.user.api.dto.request.RegisterRequest;
-import com.example.mbtogether.domain.user.api.dto.response.KakaoLinkResponse;
-import com.example.mbtogether.domain.user.api.dto.response.TokenResponse;
+import com.example.mbtogether.domain.user.api.dto.response.*;
 import com.example.mbtogether.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -23,8 +21,54 @@ public class UserController {
         return userService.getKakaoLink();
     }
 
+    @GetMapping("/auth/google")
+    public GoogleLinkResponse getGoogleLink() {
+        return userService.getGoogleLink();
+    }
+
+    @GetMapping("/auth/facebook")
+    public FacebookLinkResponse getFacebookLink() {
+        return userService.getFacebookLink();
+    }
+
     @PostMapping("/auth/kakao")
-    public TokenResponse register(@RequestBody @Valid RegisterRequest request) {
-        return userService.register(request);
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenResponse kakaoRegister(@RequestBody @Valid RegisterRequest request) {
+        return userService.kakaoRegister(request);
+    }
+
+    @PostMapping("/auth/google")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenResponse googleRegister(@RequestBody @Valid RegisterRequest request) {
+        return userService.googleRegister(request);
+    }
+
+    @PostMapping("/auth/facebook")
+    @ResponseStatus(HttpStatus.CREATED)
+    public TokenResponse facebookRegister(@RequestBody @Valid RegisterRequest request) {
+        return userService.facebookRegister(request);
+    }
+
+    @PostMapping("/kakao/login")
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponse kakaoLogin(@RequestBody @Valid LoginRequest request) {
+        return userService.kakaoLogin(request);
+    }
+
+    @PostMapping("/google/login")
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponse googleLogin(@RequestBody @Valid LoginRequest request) {
+        return userService.googleLogin(request);
+    }
+
+    @PostMapping("/facebook/login")
+    @ResponseStatus(HttpStatus.OK)
+    public LoginResponse facebookLogin(@RequestBody @Valid LoginRequest request) {
+        return userService.facebookLogin(request);
+    }
+
+    @PutMapping("/reissue")
+    public TokenResponse reissue(@RequestHeader(name = "x-refresh-token") String token) {
+        return userService.reissue(token);
     }
 }
--- src/main/java/com/example/mbtogether/domain/user/service/UserService.java
+++ src/main/java/com/example/mbtogether/domain/user/service/UserService.java
@@ -1,19 +1,29 @@
 package com.example.mbtogether.domain.user.service;
 
+import com.example.mbtogether.domain.auth.entity.RefreshToken;
+import com.example.mbtogether.domain.auth.entity.repository.RefreshTokenRepository;
+import com.example.mbtogether.domain.user.api.dto.request.LoginRequest;
 import com.example.mbtogether.domain.user.api.dto.request.RegisterRequest;
-import com.example.mbtogether.domain.user.api.dto.response.KakaoLinkResponse;
-import com.example.mbtogether.domain.user.api.dto.response.TokenResponse;
+import com.example.mbtogether.domain.user.api.dto.response.*;
 import com.example.mbtogether.domain.user.entity.User;
 import com.example.mbtogether.domain.user.repository.UserRepository;
+import com.example.mbtogether.global.exception.InvalidTokenException;
+import com.example.mbtogether.global.exception.RefreshTokenNotFoundException;
 import com.example.mbtogether.global.security.JwtTokenProvider;
-import com.example.mbtogether.infrastructure.client.KakaoAuth;
-import com.example.mbtogether.infrastructure.client.KakaoInfoClient;
-import com.example.mbtogether.infrastructure.dto.request.KakaoTokenRequest;
-import com.example.mbtogether.infrastructure.dto.response.KakaoInfoResponse;
-import com.example.mbtogether.infrastructure.dto.response.KakaoTokenResponse;
+import com.example.mbtogether.infrastructure.facebook.client.FacebookAuth;
+import com.example.mbtogether.infrastructure.facebook.client.FacebookInfoClient;
+import com.example.mbtogether.infrastructure.google.client.GoogleAuth;
+import com.example.mbtogether.infrastructure.google.client.GoogleInfoClient;
+import com.example.mbtogether.infrastructure.google.dto.request.GoogleTokenRequest;
+import com.example.mbtogether.infrastructure.google.dto.response.GoogleInfoResponse;
+import com.example.mbtogether.infrastructure.google.dto.response.GoogleTokenResponse;
+import com.example.mbtogether.infrastructure.kakao.client.KakaoAuth;
+import com.example.mbtogether.infrastructure.kakao.client.KakaoInfoClient;
+import com.example.mbtogether.infrastructure.kakao.dto.request.KakaoTokenRequest;
+import com.example.mbtogether.infrastructure.kakao.dto.response.KakaoInfoResponse;
+import com.example.mbtogether.infrastructure.kakao.dto.response.KakaoTokenResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.annotation.ReadOnlyProperty;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -26,11 +36,15 @@ import java.nio.charset.StandardCharsets;
 public class UserService {
 
     private static final String KAKAO_LOGIN_LINK = "https://kauth.kakao.com/oauth/authorize";
+    private static final String GOOGLE_LOGIN_LINK = "https://accounts.google.com/o/oauth2/v2/auth";
 
     private final KakaoAuth kakaoAuth;
     private final KakaoInfoClient kakaoInfoClient;
+    private final GoogleAuth googleAuth;
+    private final GoogleInfoClient googleInfoClient;
     private final UserRepository userRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
 
     @Value("${oauth.kakao.client-id}")
     private String kakaoClientId;
@@ -38,15 +52,31 @@ public class UserService {
     @Value("${oauth.kakao.redirect-uri}")
     private String kakaoRedirectUri;
 
+    @Value("${oauth.google.client-id}")
+    private String googleClientId;
+
+    @Value("${oauth.google.redirect-uri}")
+    private String googleRedirectUri;
+
     public KakaoLinkResponse getKakaoLink() {
         return new KakaoLinkResponse(KAKAO_LOGIN_LINK +
                 "?client_id=" + kakaoClientId +
                 "&redirect_uri=" + URLEncoder.encode(kakaoRedirectUri, StandardCharsets.UTF_8) +
-                "&response_type=code"
+                "&response_type=code" +
+                "&scope=https://www.googleapis.com/auth/drive.metadata.readonly"
         );
     }
 
-    public TokenResponse register(RegisterRequest request) {
+    public GoogleLinkResponse getGoogleLink() {
+        return new GoogleLinkResponse(GOOGLE_LOGIN_LINK +
+                "?client_id=" + googleClientId +
+                "&redirect_uri=" + URLEncoder.encode(googleRedirectUri, StandardCharsets.UTF_8) +
+                "&response_type=code" +
+                "&scope=https://www.googleapis.com/auth/drive.metadata.readonly");
+    }
+
+    @Transactional
+    public TokenResponse kakaoRegister(RegisterRequest request) {
         KakaoTokenResponse response = kakaoAuth.getTokenByCode(
                 new KakaoTokenRequest(URLDecoder.decode(request.getCode(), StandardCharsets.UTF_8),
                         kakaoClientId, kakaoRedirectUri, "authorization_code")
@@ -67,10 +97,88 @@ public class UserService {
         return getToken(oauthId);
     }
 
+    @Transactional
+    public TokenResponse googleRegister(RegisterRequest request) {
+        GoogleTokenResponse response = googleAuth.getTokenByCode(
+                new GoogleTokenRequest(URLDecoder.decode(request.getCode(), StandardCharsets.UTF_8),
+                        googleClientId, googleRedirectUri, "authorization_code")
+        );
+
+        GoogleInfoResponse info = googleInfoClient.getUserInfo("Bearer" + response.getAccessToken());
+        String oauthId = info.getEmail();
+
+        if (userRepository.findByOauthId(oauthId).isEmpty()) {
+            userRepository.save(
+                    User.builder()
+                            .oauthId(oauthId)
+                            .oauthType("GOOGLE")
+                            .nickname(request.getNickname())
+                            .build()
+            );
+        }
+        return getToken(oauthId);
+    }
+
+    @Transactional
+    public LoginResponse kakaoLogin(LoginRequest request) {
+        KakaoTokenResponse response = kakaoAuth.getTokenByCode(
+                new KakaoTokenRequest(URLDecoder.decode(request.getCode(), StandardCharsets.UTF_8),
+                        kakaoClientId, kakaoRedirectUri, "authorization_code")
+        );
+
+        KakaoInfoResponse info = kakaoInfoClient.getUserInfo("Bearer" + response.getAccessToken());
+        String oauthId = info.getOauthId().toString();
+
+        if (userRepository.findByOauthId(oauthId).isEmpty())
+            return new LoginResponse(false, null, null);
+
+        TokenResponse token = getToken(oauthId);
+        return new LoginResponse(true, token.getAccessToken(), token.getRefreshToken());
+    }
+
+    @Transactional
+    public LoginResponse googleLogin(LoginRequest request) {
+        GoogleTokenResponse response = googleAuth.getTokenByCode(
+                new GoogleTokenRequest(URLDecoder.decode(request.getCode(), StandardCharsets.UTF_8),
+                        googleClientId, googleRedirectUri, "authorization_code")
+        );
+
+        GoogleInfoResponse info = googleInfoClient.getUserInfo("Bearer" + response.getAccessToken());
+        String oauthId = info.getEmail();
+
+        if (userRepository.findByOauthId(oauthId).isEmpty())
+            return new LoginResponse(false, null, null);
+
+        TokenResponse token = getToken(oauthId);
+        return new LoginResponse(true, token.getAccessToken(), token.getRefreshToken());
+    }
+
+    @Transactional
+    public TokenResponse reissue(String token) {
+        if (!jwtTokenProvider.validateRefreshToken(token)) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+
+        RefreshToken newRefreshToken = refreshTokenRepository.findByRefreshToken(token)
+                .map(refreshToken -> refreshTokenRepository.save(
+                        refreshToken.update(172800L)
+                )).orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+
+        return new TokenResponse(jwtTokenProvider.generateAccessToken(newRefreshToken.getOauthId()), token);
+    }
+
     private TokenResponse getToken(String oauthId) {
 
         String accessToken = jwtTokenProvider.generateAccessToken(oauthId);
         String refreshToken = jwtTokenProvider.generateRefreshToken(oauthId);
+
+        refreshTokenRepository.save(
+                RefreshToken.builder()
+                        .oauthId(oauthId)
+                        .refreshToken(refreshToken)
+                        .refreshExpiration(172800L)
+                        .build()
+        );
 
         return new TokenResponse(accessToken, refreshToken);
     }
