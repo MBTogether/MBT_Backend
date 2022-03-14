60232c14e229a5ab45b511e02421220a9d908c8b
이정윤 <dlwjddbs14@naver.com>
2022-03-14 09:18:57 +0900

    ✅ :: @Id 수정

--- src/main/java/com/example/mbtogether/domain/auth/entity/RefreshToken.java
+++ src/main/java/com/example/mbtogether/domain/auth/entity/RefreshToken.java
@@ -1,11 +1,11 @@
 package com.example.mbtogether.domain.auth.entity;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 import org.springframework.data.redis.core.index.Indexed;
 
-import javax.persistence.Id;
 import java.io.Serializable;
 
 @Getter
